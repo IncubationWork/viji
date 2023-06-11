ab55307d0f2e1797019ee154ab889a30a99689cd
Tulipnila <vijibalan10@gmail.com>
2023-06-11 23:48:04 +0530

    output

--- internjs/Assignment-04/script.js
+++ internjs/Assignment-04/script.js
@@ -2,80 +2,71 @@
 let list = document.getElementById('list');
 let filter = document.querySelector('.filter');
 let count = document.getElementById('count');
+let filterIcon = document.getElementById('search')
 let listProducts = [
 {
 id: 1,
-name: 'Name Product white-black',
+name: 'Name Product skyblue',
 price: 1200,
 quantity: 0,
 image: 'images/img1.png',
-nature:  { 
-    color: ['white','black'],
-    size: ['S','M','L'],
-    type: 'frock'
-}
+
 },
 {
 id:2,
-name: 'Name Product white-black-grey',
+name: 'Name Product blue',
 price: 750,
 quantity:20,
 image:'images/img2.png',
-nature:{
-    color:['white','black','grey'],
-    size: ['S','M','L'],
-    type:'frock'
-}
+
 },
 {
 id:3,
-name: 'Name Product blue-black',
+name: 'Name Product peach',
 price: 1176,
 quantity:30,
 image:'images/img3.png',
-nature:{
-    color:['blue','black'],
-    size: ['S','M','L'],
-    type:'frock'
-}
+
 },
 {
 id:4,
-name: 'Name Product red-black',
+name: 'Name Product red-white',
 price: 845,
 quantity:30,
 image:'images/img4.png',
-nature:{
-    color:['red','black'],
-    size: ['S','M','L'],
-    type:'tracks'
-}
+
 },
 {
 id:5,
-name: 'Name Product brown-black',
+name: 'Name Product multi-color',
 price: 567,
 quantity:30,
 image:'images/img5.png',
-nature:{
-    color:['black'],
-    size: ['S','M','L'],
-    type:'shirt'
-}
+
 },
 {
 id:6,
-name: 'Name Product white-black',
+name: 'Name Product white',
 price: 768,
 quantity:30,
 image:'images/img6.png',
-nature:{
-    color:['white','black'],
-    size: ['S','M','L'],
-    type:'pants'
-}
-},
 
+},
+{
+    id:7,
+    name: 'Name Product orange',
+    price: 1350,
+    quantity:30,
+    image:'images/img7.png',
+   
+    },
+    {
+        id:8,
+        name: 'Name Product orange',
+        price: 1350,
+        quantity:30,
+        image:'images/img8.png',
+    }   
 ];
 let productFilter = listProducts;
 showProduct(productFilter);
@@ -110,16 +101,9 @@ event.preventDefault();
 let valueFilter = event.target.elements;
 productFilter = listProducts.filter(item =>{
 
-//check minprice
-if(valueFilter.minPrice.value !=''){
-    if(item.price < valueFilter.minPrice.value){
-        return false;
-         
-    }
-}
 //check maxprice
-if(valueFilter.maxPrice.value !=''){
-    if(item.price > valueFilter.maxPrice.value){
+if(valueFilter.Price.value !=''){
+    if(item.price > valueFilter.Price.value){
         return false;
     }
 }
@@ -128,4 +112,3 @@ return true;
 showProduct(productFilter);
 
 })
-
