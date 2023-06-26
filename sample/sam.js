b6bb31688a2ef2c06064a6aa367f9acc60775e9d
Tulipnila <vijibalan10@gmail.com>
2023-06-26 12:49:23 +0530

    sam.js

--- sample/sam.js
+++ sample/sam.js
@@ -1,24 +1,24 @@
       
   class Person{
-      constructor(name,age,sex){
+    constructor(name,age,sex){
       this.name = name;
       this.age = age;
       this.sex = sex;
+    }
+    getVoiceType(){
+      if(this.age>=18 && this.age<=40 && this.sex==="male"){
+        return "hoarse";
+      }else if(this.age>= 18 && this.age<=30 && this.sex === "female"){
+        return "Contralto tessitura";
       }
-      getVoiceType(){
-          if(this.age>=18 && this.age<=40 && this.sex==="male"){
-            return "hoarse";
-          }else if(this.age>= 18 && this.age<=30 && this.sex === "female"){
-            return "Contralto tessitura";
-          }
-          else if (this.age<18){
-            return "soft";
-          }
-          else {
-            return "tenor"
-          }
-        }
+      else if (this.age<18){
+        return "soft";
       }
+      else {
+        return "tenor"
+      }
+    }
+  }
       
   class Employee extends Person{
     constructor(id,name,age,sex){
@@ -27,11 +27,11 @@
     }
   }
 
-      let a = new Employee(1,"tulip", 52, "female");
-      let b = new Employee(2, "ram", 32, "male");
+  let a = new Employee(1,"tulip", 52, "female");
+  let b = new Employee(2, "ram", 32, "male");
 
- const aPrototype = Object.getPrototypeOf(a);
- const bPrototype = Object.getPrototypeOf(b);
+  const aPrototype = Object.getPrototypeOf(a);
+  const bPrototype = Object.getPrototypeOf(b);
  
       
   console.log(aPrototype.getVoiceType === bPrototype.getVoiceType);
